7271e415aa6b7379694b1e0df6c92cfa095ced52
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-05-26 20:05:20 +0800

    initial commit

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+	require_once("common/imgprocess.php");
+	$name=$_GET['name'];
+	$text=$_GET['text'];
+	if (!isset($name)) {
+		die();
+	}
+	$img=loadimg($name);
+	addtext($img['img'], $text, $img['basey'],$img['ymax'],$img['maxsize']);
+	output($img['img']);
+?>
---
+++ common/imgprocess.php
@@ -0,0 +1,35 @@
+<?php
+	function loadimg($fn) {
+		$img=imagecreatefromjpeg("img/".$fn.".jpg");
+		if ($img==false) {
+			echo "error:invalid name";
+			die();
+		}
+		$config=json_decode(fread(fopen("img/".$fn.".json","r"),filesize("img/".$fn.".json")),true);
+		$result=[
+			"img" => $img,
+			"basey" => $config['basey'],
+			"ymax" => $config['ymax'],
+			"maxsize" => $config['maxsize']
+		];
+		return $result;
+	}
+	function addtext($img,$text,$basey,$ymax,$sizemax) {
+		$black = imagecolorallocate($img, 0x00, 0x00, 0x00);
+		putenv('GDFONTPATH=' . realpath('./font/'));
+		$size=$sizemax;
+		$ylen=999;
+		while (($ylen>$ymax) or ($x<0)) {
+			$size-=10;
+			$bbox=imageftbbox($size, 0, "HYH3GJM.TTF", $text);
+			$ylen=$bbox[3]-$bbox[5];
+			$x = $bbox[0] + (imagesx($img) / 2) - ($bbox[4] / 2) - 5;
+			$y = $basey-(ylen/2);
+		}
+	$result=imagefttext($img, $size, 0, $x, $y, $black, "HYH3GJM.TTF", $text);
+	}
+	function output($img) {
+		header('Content-Type: image/png');
+		imagejpeg($img);
+	}
+?>
