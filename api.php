9f174c36dde22006faa6ae6080dc0f8d4b22ecaa
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-05-27 21:24:13 +0800

    oop refactoring

--- api.php
+++ api.php
@@ -3,9 +3,11 @@
 	$name=$_GET['name'];
 	$text=$_GET['text'];
 	if (!isset($name)) {
+		http_response_code(404);
 		die();
 	}
-	$img=loadimg($name);
-	addtext($img['img'], $text, $img['basey'],$img['ymax'],$img['maxsize'],$_GET['color']);
-	output($img['img']);
+	$img=new PandaImage();
+	$img->loadimg($name);
+	$img->addtext($text, $_GET['color']);
+	$img->output();
 ?>
--- common/imgprocess.php
+++ common/imgprocess.php
@@ -1,5 +1,8 @@
 <?php
-	function hex2rgb($hexColor,$img){
+class PandaImage {
+	private $img;
+	private $config;
+	function hex2col($hexColor){
 		$color=str_replace('#','',$hexColor);
 		if (strlen($color)> 3){
 			$r=hexdec(substr($color,0,2));
@@ -11,43 +14,42 @@
 			$g=hexdec(substr($color,1,1).substr($color,1,1));
 			$b=hexdec(substr($color,2,1).substr($color,2,1));
 		}
-		return imagecolorallocate($img, $r, $g, $b);
+		return imagecolorallocate($this->img, $r, $g, $b);
 	}
-	function loadimg($fn) {
+	public function loadimg($fn) {
 		if (strpos($fn,"..") or strpos($fn,"/")) {
 			echo "error:invalid name";
+			http_response_code(403);
 			die();
 		}
-		$img=imagecreatefromjpeg("img/".$fn.".jpg");
-		if ($img==false) {
+		$this->img=imagecreatefromjpeg("img/".$fn.".jpg");
+		if ($this->img==false) {
 			echo "error:invalid name";
+			http_response_code(404);
 			die();
 		}
-		$config=json_decode(fread(fopen("img/".$fn.".json","r"),filesize("img/".$fn.".json")),true);
-		$result=[
-			"img" => $img,
-			"basey" => $config['basey'],
-			"ymax" => $config['ymax'],
-			"maxsize" => $config['maxsize']
-		];
-		return $result;
+		$this->config=json_decode(fread(fopen("img/".$fn.".json","r"),filesize("img/".$fn.".json")),true);
 	}
-	function addtext($img,$text,$basey,$ymax,$sizemax,$col) {
-		$color = hex2rgb($col, $img);
+	public function addtext($text,$col) {
+		$basey=$this->config['basey'];
+		$ymax=$this->config['ymax'];
+		$maxsize=$this->config['maxsize'];
+		$color = $this->hex2col($col, $this->img);
 		putenv('GDFONTPATH=' . realpath('./font/'));
-		$size=$sizemax;
+		$size=$maxsize;
 		$ylen=999;
 		while (($ylen>$ymax) or ($x<0)) {
 			$size-=5;
 			$bbox=imageftbbox($size, 0, "HYH3GJM.TTF", $text);
 			$ylen=$bbox[3]-$bbox[5];
-			$x = $bbox[0] + (imagesx($img) / 2) - ($bbox[4] / 2);
+			$x = $bbox[0] + (imagesx($this->img) / 2) - ($bbox[4] / 2);
 			$y = $basey-($ylen/2);
 		}
-		$result=imagefttext($img, $size, 0, $x, $y, $color, "STXINWEI.TTF", $text);
+		$result=imagefttext($this->img, $size, 0, $x, $y, $color, "STXINWEI.TTF", $text);
 	}
-	function output($img) {
+	public function output() {
 		header('Content-Type: image/jpeg');
-		imagejpeg($img);
+		imagejpeg($this->img);
 	}
+}
 ?>
